711e167b2b21db53ac1f341c93bc9f83368dadcd
KakolJakub <kakoljakub@wp.pl>
2021-06-04 02:49:28 +0200

    Initial commit

---
+++ app/src/main/java/com/example/firebase/Main.java
@@ -0,0 +1,36 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Notepad");
+        
+        primaryStage.setScene(new Scene(root));
+        Screen screen = Screen.getPrimary();
+        
+
+        primaryStage.setX(bounds.getMinX());
+        primaryStage.setY(bounds.getMinY());
+        primaryStage.setWidth(bounds.getWidth());
+        primaryStage.setHeight(bounds.getHeight());
+        primaryStage.setMaximized(true);
+        primaryStage.setMaximized(true);
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
